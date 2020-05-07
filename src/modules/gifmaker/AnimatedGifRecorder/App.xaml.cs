cc1598e638d1bbee46611db855ccb08ac983a8c9
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-07 11:12:02 -0400

    Ported Capture Utilities from ScreenToGif

--- src/modules/gifmaker/AnimatedGifRecorder/App.xaml.cs
+++ src/modules/gifmaker/AnimatedGifRecorder/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using AnimatedGifRecorder.ViewModels;
 
 namespace AnimatedGifRecorder
 {
@@ -13,5 +14,27 @@ namespace AnimatedGifRecorder
     /// </summary>
     public partial class App : Application
     {
+        private MainWindow _mainWindow;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+                
+            // standard MVVM binding
+            _mainWindow = new MainWindow();
+            _mainWindow.DataContext = new MainViewModel();
+            _mainWindow.Show();
+        }
+
+        /// <summary>
+        /// Application wide error handling
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
